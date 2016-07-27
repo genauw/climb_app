7ee9a1153c548833c42f6305ba5dc0424dce7fa2
david lutkin <genauw@gmail.com>
2016-07-27 17:50:48 +0200

    adding basic home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'index_pages/home'
+  root 'index_pages#home'
 
+  get 'index_pages/home'
   get 'index_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
